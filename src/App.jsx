9aaa5a626f0fb08c1095e6db652632c99d13d445
Maryana Ghabyous <maremar3@hotmail.com>
2024-03-17 23:38:11 -0700

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
 import Gallery from './components/Gallery'
+import Nav from './components/Nav'
 //import main from './main'
 import './App.css'
 
 
 function App() {
   const [count, setCount] = useState(0)
-  //console.log(import.meta.env.VITE_API_Key);
+  console.log(import.meta.env.VITE_API_Key);
   return (
     <>
 MY APP
 
-
+<Nav />
 <Routes>
 {/* 
     <Route path='/' element={<Main />} />
     */}
-     <Route path='/Gallery' element={<Gallery />} />
+     <Route path='Gallery' element={<Gallery />} />
 
       </Routes>
        
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,17 +1,16 @@
 import React, {useState,useEffect}from 'react'
 
 
-// import {apikey}   from    '../apikey.jsx'
 import {apikey} from   '../apikey.jsx'
 export default function Gallery(){
     const [galleryResults,setGalleryResults] =useState([])
 useEffect(async ()=>{
     const headers = {
-       ' Authorization': apikey
+       "Authorization": apikey
         
       }
      const apiResponse = await  fetch('https://api.pexels.com/v1/search?query=people', { headers });
-      console.log(apiResponse)
+    //   console.log("API RESPONSE  :  "+apiResponse)
 
 },[])
 return
