8dfb0d2367903bf1f5f3dc3ff10fe4713ff7873c
Maryana Ghabyous <maremar3@hotmail.com>
2024-03-17 23:40:45 -0700

    no new

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,7 +6,7 @@ export default function Gallery(){
     const [galleryResults,setGalleryResults] =useState([])
 useEffect(async ()=>{
     const headers = {
-       "Authorization": apikey
+       'Authorization': apikey
         
       }
      const apiResponse = await  fetch('https://api.pexels.com/v1/search?query=people', { headers });
